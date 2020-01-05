379c42834ce1c2f272acca9422a3c54fcca941f4
Athar Imam <atharandaftab@hotmail.com>
2020-01-06 02:21:15 +0530

    Upload youtube downloader

---
+++ YoutubeDownloader/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace YoutubeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Youtube URL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "File Path";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(145, 37);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(471, 20);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(145, 102);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(471, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(295, 151);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_ClickAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 196);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnDownload;
+    }
+}
+
---
+++ YoutubeDownloader/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeExplode.Models;
+
+namespace YoutubeDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnDownload_ClickAsync(object sender, EventArgs e)
+        {
+            await this.DownloadVideo();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private async Task DownloadVideo()
+        {
+            YoutubeClient client = new YoutubeClient();
+
+            if (txtUrl.Text.IndexOf("&list=") > 0)
+            {
+                string id = txtUrl.Text.Split(new string[] { "&list=" }, StringSplitOptions.RemoveEmptyEntries)[1];
+                var playlist = await client.GetPlaylistAsync(id);
+                int count = playlist.Videos.Count();
+                if (count > 0)
+                {
+                    int index = 1;
+                    foreach (var vid in playlist.Videos)
+                    {
+                        await this.DownloadVideo(vid.Id, $"{index} / {count}");
+                        index++;
+                    }
+                }
+            }
+            else
+            {
+                var id = YoutubeClient.ParseVideoId(txtUrl.Text);
+                await this.DownloadVideo(id);
+            }
+
+            MessageBox.Show("Succeeded");
+        }
+
+        private async Task DownloadVideo(string id, string info = "")
+        {
+            YoutubeClient client = new YoutubeClient();
+
+            Video video = await client.GetVideoAsync(id);
+            this.Text = $"{info} Downloading Video : {video.Title}";
+            // Get metadata for all streams in this video
+            var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(id);
+
+            // Select one of the streams, e.g. highest quality muxed stream
+            var streamInfo = streamInfoSet.Muxed.First(item => item.VideoQuality == streamInfoSet.Muxed.Max(itm => itm.VideoQuality));
+
+            // Get file extension based on stream's container
+            var ext = streamInfo.Container.ToString();
+
+            string filePath = video.Title + "." + ext;
+
+            filePath = filePath.Replace("/", "");
+
+            string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+
+            foreach (char c in invalid)
+            {
+                filePath = filePath.Replace(c.ToString(), "");
+            }
+
+            filePath = Path.Combine(txtPath.Text, filePath);
+
+            // Download stream to file
+            await client.DownloadMediaStreamAsync(streamInfo, filePath);
+        }
+    }
+}
